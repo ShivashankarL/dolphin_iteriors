002fe8f4d1b2e1dd8042b5c80e9b06bb11ec9a12
Shivashankar L <ss.lakshminarayan@myeduate.com>
2023-05-14 22:25:52 +0530

    Initial Commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./css/LandingPage.css";
+import NavBar from "./NavBar";
+import logo from "../images/logo.svg";
+import title1 from "../images/Dolphindream_Infra_Projects_LLP.svg";
+const LandingPage = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="landing_page">
+        <img src={logo} alt=" not found" />
+        <img src={title1} alt=" not found" />
+        <button
+          onClick={() => {
+            alert("To be implemented");
+          }}
+          className="get-quote-btn"
+        >
+          Get a Quote!
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
---
+++ src/components/NavBar.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./css/NavBar.css";
+import Logo from "../images/navbar_logo.png";
+const NavBar = () => {
+  // const am = document.getElementsByClassName("menu-toggle")[0];
+  const [class_name, setClassName] = useState({
+    nav: "nav",
+    icon: "menu-toggle",
+  });
+
+  const toggleClassName = () => {
+    setClassName(
+      class_name.nav === "nav"
+        ? {
+            nav: "mobile-nav",
+            icon: "is-active",
+          }
+        : {
+            nav: "nav",
+            icon: "menu-toggle",
+          }
+    );
+  };
+  window.onscroll = () => {
+    setClassName({
+      nav: "nav",
+      icon: "menu-toggle",
+    });
+  };
+  return (
+    <div className="page-wrapper">
+      <div className="nav-wrapper">
+        <div className="grad-bar"></div>
+        <nav className="navbar">
+          <img src={Logo} alt="Company Logo" />
+          <div className="logo_title">
+            Dolphin<span className="sublogo_title">Interiors</span>
+          </div>
+          <div
+            className={class_name.icon}
+            id="mobile-menu"
+            onClick={toggleClassName}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <ul
+            className={`${class_name.nav} no-search`}
+            onClick={toggleClassName}
+          >
+            <li className="nav-item">
+              <a href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a href="#">Work</a>
+            </li>
+            <li className="nav-item">
+              <a href="#">Careers</a>
+            </li>
+            <li className="nav-item">
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="grad-bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
