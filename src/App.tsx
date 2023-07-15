b5e7a892abee8d99ff49365b35bfde844dd64e26
Shivashankar L <ss.lakshminarayan@myeduate.com>
2023-07-15 23:31:04 +0530

    Gallery In Progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import "./App.css";
 import React from "react";
 import LandingPage from "./components/LandingPage";
 import NavBar from "./components/NavBar";
 import GetQuote from "./components/GetQuote";
+import Gallery from "./components/Gallery";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import { getTokenFound } from "../src/firebase/firebaseconfig.js";
 
 function App() {
   // const [tokenFound, setTokenFound] = useState(false);
   // getTokenFound(setTokenFound);
   // console.log(tokenFound, "TokenFound");
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <NavBar>
+          <LandingPage />
+        </NavBar>
+      ),
+    },
+    {
+      path: "/gallery",
+      element: (
+        <NavBar>
+          <Gallery />
+        </NavBar>
+      ),
+    },
+  ]);
   return (
     <>
-      <NavBar />
-      <LandingPage />
-      <GetQuote />
+      <RouterProvider router={routes} />
+      {/* <GetQuote />
+      <Gallery /> */}
     </>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import "./css/NavBar.css";
 import Logo from "../images/logo.svg";
-const NavBar = () => {
+import { Link } from "react-router-dom";
+interface Props {
+  children: React.ReactNode;
+}
+const NavBar: React.FC<Props> = ({ children }) => {
   // const am = document.getElementsByClassName("menu-toggle")[0];
   const [class_name, setClassName] = useState({
     nav: "nav",
@@ -28,47 +32,47 @@ const NavBar = () => {
     });
   };
   return (
-    <div className="page-wrapper">
-      <div className="nav-wrapper">
-        <div className="grad-bar"></div>
-        <nav className="navbar">
-          <img src={Logo} alt="Company Logo" />
-          <div className="logo_title">
-            Dolphin<span className="sublogo_title">Interiors</span>
-          </div>
-          <div
-            className={class_name.icon}
-            id="mobile-menu"
-            onClick={toggleClassName}
-          >
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
-          <ul
-            className={`${class_name.nav} no-search`}
-            onClick={toggleClassName}
-          >
-            <li className="nav-item">
-              <a href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a href="#">Work</a>
-            </li>
-            <li className="nav-item">
-              <a href="#">Careers</a>
-            </li>
-            <li className="nav-item">
-              <a href="#">Contact Us</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="grad-bar"></div>
+    <>
+      <div className="page-wrapper">
+        <div className="nav-wrapper">
+          <div className="grad-bar"></div>
+          <nav className="navbar">
+            <img src={Logo} alt="Company Logo" />
+            <div className="logo_title">
+              Dolphin<span className="sublogo_title">Interiors</span>
+            </div>
+            <div
+              className={class_name.icon}
+              id="mobile-menu"
+              onClick={toggleClassName}
+            >
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </div>
+            <ul className={`${class_name.nav} no-search`}>
+              <li className="nav-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/gallery">Gallery</Link>
+              </li>
+              <li className="nav-item">
+                <a href="#">Work</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Careers</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="grad-bar"></div>
+        </div>
       </div>
-    </div>
+      {children}
+    </>
   );
 };
 
