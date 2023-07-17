9c71215211e706cfe97adf72f591ac6b9d0be460
Shivashankar L <ss.lakshminarayan@myeduate.com>
2023-07-17 20:52:07 +0530

    setup gh-pages

---
+++ src/components/GalleryIntro.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Kitchen from "../images/kitchen_cat.jpg";
+
+const GalleryIntro = () => {
+  return (
+    <div className="gallery_intro">
+      <h1>Design Gallery!</h1>
+      <div className="gallery_category">
+        <div className="ima">
+          <img src={Kitchen} alt="Image not Found" />
+          <span className="cat_title">Kitchen</span>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default GalleryIntro;
