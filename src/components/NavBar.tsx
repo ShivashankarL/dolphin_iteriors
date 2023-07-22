347e94f4d63e01f3e2916ab3e0e64f8ec676d4dc
Shivashankar L <ss.lakshminarayan@myeduate.com>
2023-07-22 23:45:33 +0530

    Nav Bar Changed from css to Scss

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./css/NavBar.css";
+import "./css/NavBar1.scss";
 import Logo from "../images/logo.svg";
 import { Link } from "react-router-dom";
 interface Props {
@@ -28,8 +28,6 @@ const NavBar: React.FC<Props> = ({ children }) => {
   console.log(class_name.icon);
 
   window.onscroll = () => {
-    console.log("Helo");
-
     setClassName({
       nav: "nav",
       icon: "menu-toggle",
@@ -37,13 +35,12 @@ const NavBar: React.FC<Props> = ({ children }) => {
   };
   return (
     <>
-      <div className="page-wrapper">
-        <div className="nav-wrapper">
-          <div className="grad-bar"></div>
-          <nav className="navbar">
-            <img src={Logo} alt="Company Logo" />
-            <div className="logo_title">
-              Dolphin<span className="sublogo_title">Interiors</span>
+      <div className="navbar">
+        <div className="navbar-nav-wrapper">
+          <nav className="navbar-nav">
+            <img src={Logo} alt="Company Logo" className="navbar-logo-img" />
+            <div className="logo-title">
+              Dolphin<span className="navbar-subtitle">Interiors</span>
             </div>
             <div
               className={"menu-toggle"}
@@ -62,17 +59,10 @@ const NavBar: React.FC<Props> = ({ children }) => {
                 <Link to="/gallery">Gallery</Link>
               </li>
               <li className="nav-item">
-                <a href="#">Work</a>
-              </li>
-              <li className="nav-item">
-                <a href="#">Careers</a>
-              </li>
-              <li className="nav-item">
                 <a href="#">Contact Us</a>
               </li>
             </ul>
           </nav>
-          <div className="grad-bar"></div>
         </div>
       </div>
       {children}
