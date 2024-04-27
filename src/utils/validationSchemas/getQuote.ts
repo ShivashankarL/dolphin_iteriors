e309c271f54ff3dcd8f667181388dab326c4b96f
ShivashankarL <ss.lakshminarayan@myeduate.com>
2024-04-27 22:12:33 +0530

    Updates

--- src/utils/validationSchemas/getQuote.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup'
-
-
-export const GetAQuoteValidationSchema = Yup.object({
-    client_email: Yup.string().email("Invalid email").required("Email Missing!"),
-    client_name: Yup.string().required("FullName Missing!"),
-    client_phone: Yup.string().required("Contact No. Missing!"),
-    client_requirement: Yup.string().required("Requirment is Missing!"),
-})
